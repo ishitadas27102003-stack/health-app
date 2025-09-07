a550b8428d75d9f557b1df4cd06164d76b187db2
ishitadas27102003-stack <ishitadas27102003@gmail.com>
2025-09-07 14:14:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("this is my first web application in python")
+st.header("i'll try my best to learn python for AI")
+st.subheader("this my first attempt to create web app")
+st.write("hello everyone")
+st.button("press now")
+st.write("hello")
