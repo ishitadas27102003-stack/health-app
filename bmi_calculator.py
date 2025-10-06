34d4b5620eadee469f9db32334eb7ee3b85ca466
ishitadas27102003-stack <ishitadas27102003@gmail.com>
2025-10-06 12:31:21 +0530

    Add files via upload

---
+++ bmi_calculator.py
@@ -0,0 +1,20 @@
+import google.generativeai as genai
+import streamlit as st
+genai.configure(api_key="AIzaSyD3G__5I9EA9eHC3myDGEvj94uaEYwwkyM")
+model=genai.GenerativeModel(model_name="gemini-2.5-flash-lite")
+st.title("AI BASED BMI CALCULATOR-KNOW YOUR HEALTH APP")
+# BMI CALCULATOR
+# TAKE INPUTS FROM THE USER
+name=st.text_input("ENTER YOUR NAME:")
+wt=st.number_input("ENTER YOUR WEIGHT IN KG:")
+ht=st.slider("ENTER YOUR HEIGHT IN CM",50,200)
+age=st.number_input("ENTER YOUR AGE IN YEARS")
+gender=st.text_input("ENTER YOUR GENDER:")
+#CALCULATE THE BMI 
+bmi=round(wt/(ht/100)**2,2)
+st.write(f"{name}, your bmi is {bmi}")
+prompt = f"act like an expert nutritionist,comment on the bmi with the following data: height as {ht},weight as {wt},and bmi as {bmi} as BMI is:"
+response=model.generate_content(prompt)
+st.markdown(response.text)
+
+
